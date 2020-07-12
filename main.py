f4039359101f174bed7c5aac432649e1cf64ed8f
Atharv Sonwane <atharvs.twm@gmail.com>
2020-07-12 22:18:52 +0530

    Added individual plotting

--- main.py
+++ main.py
@@ -19,23 +19,23 @@ EPOCHS = 20
 EPISODES_PER_EPOCH = 16
 
 vpg_model = vpg.VPG(env)
-# vpg_model.train(
-#     epochs=EPOCHS,
-#     episodes_per_epoch=EPISODES_PER_EPOCH,
-#     # value_lr=VALUE_FN_LEARNING_RATE,
-#     # policy_lr=POLICY_LEARNING_RATE,
-#     # gamma=GAMMA,
-#     # max_traj_length=MAX_TRAJ_LENGTH,
-#     log_dir=log_dir,
-#     # RENDER=False,
-#     PLOT_REWARDS=True,
-#     VERBOSE=True,
-#     TENSORBOARD_LOG=True,
-#     SHOW_PLOTS=False,
-# )
-# vpg_model.save(
-#     checkpoints_path.joinpath(f"vpg_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt")
-# )
+vpg_model.train(
+    epochs=EPOCHS,
+    episodes_per_epoch=EPISODES_PER_EPOCH,
+    # value_lr=VALUE_FN_LEARNING_RATE,
+    # policy_lr=POLICY_LEARNING_RATE,
+    # gamma=GAMMA,
+    # max_traj_length=MAX_TRAJ_LENGTH,
+    log_dir=log_dir,
+    # RENDER=False,
+    PLOT_REWARDS=True,
+    VERBOSE=True,
+    TENSORBOARD_LOG=True,
+    SHOW_PLOTS=False,
+)
+vpg_model.save(
+    checkpoints_path.joinpath(f"vpg_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt")
+)
 
 causal_pg_model = vpg.CausalPG(compute_causal_factor1, env)
 causal_pg_model.train(
@@ -59,55 +59,89 @@ causal_pg_model.save(
 )
 
 ppo_model = ppo.PPO(env)
-# ppo_model.train(
-#     epochs=EPOCHS,
-#     episodes_per_epoch=EPISODES_PER_EPOCH,
-#     # n_value_updates=N_VALUE_UPDATES,
-#     # n_policy_updates=N_POLICY_UPDATES,
-#     # value_lr=VALUE_FN_LEARNING_RATE,
-#     # policy_lr=POLICY_LEARNING_RATE,
-#     # gamma=GAMMA,
-#     # epsilon=EPSILON,
-#     # max_traj_length=MAX_TRAJ_LENGTH,
-#     log_dir=log_dir,
-#     # RENDER=False,
-#     PLOT_REWARDS=True,
-#     VERBOSE=True,
-#     TENSORBOARD_LOG=True,
-#     SHOW_PLOTS=False,
-# )
-# ppo_model.save(
-#     checkpoints_path.joinpath(f"ppo_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt")
-# )
+ppo_model.train(
+    epochs=EPOCHS,
+    episodes_per_epoch=EPISODES_PER_EPOCH,
+    # n_value_updates=N_VALUE_UPDATES,
+    # n_policy_updates=N_POLICY_UPDATES,
+    # value_lr=VALUE_FN_LEARNING_RATE,
+    # policy_lr=POLICY_LEARNING_RATE,
+    # gamma=GAMMA,
+    # epsilon=EPSILON,
+    # max_traj_length=MAX_TRAJ_LENGTH,
+    log_dir=log_dir,
+    # RENDER=False,
+    PLOT_REWARDS=True,
+    VERBOSE=True,
+    TENSORBOARD_LOG=True,
+    SHOW_PLOTS=False,
+)
+ppo_model.save(
+    checkpoints_path.joinpath(f"ppo_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt")
+)
 
 causal_ppo_model = ppo.CausalPPO(compute_causal_factor1, env)
-# causal_ppo_model.train(
-#     epochs=EPOCHS,
-#     episodes_per_epoch=EPISODES_PER_EPOCH,
-#     # n_value_updates=N_VALUE_UPDATES,
-#     # n_policy_updates=N_POLICY_UPDATES,
-#     # value_lr=VALUE_FN_LEARNING_RATE,
-#     # policy_lr=POLICY_LEARNING_RATE,
-#     # gamma=GAMMA,
-#     # epsilon=EPSILON,
-#     # max_traj_length=MAX_TRAJ_LENGTH,
-#     log_dir=log_dir,
-#     # RENDER=False,
-#     PLOT_REWARDS=True,
-#     VERBOSE=True,
-#     TENSORBOARD_LOG=True,
-#     SHOW_PLOTS=False,
-# )
-# causal_ppo_model.save(
-#     checkpoints_path.joinpath(
-#         f"causal_pg_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt"
-#     )
-# )
+causal_ppo_model.train(
+    epochs=EPOCHS,
+    episodes_per_epoch=EPISODES_PER_EPOCH,
+    # n_value_updates=N_VALUE_UPDATES,
+    # n_policy_updates=N_POLICY_UPDATES,
+    # value_lr=VALUE_FN_LEARNING_RATE,
+    # policy_lr=POLICY_LEARNING_RATE,
+    # gamma=GAMMA,
+    # epsilon=EPSILON,
+    # max_traj_length=MAX_TRAJ_LENGTH,
+    log_dir=log_dir,
+    # RENDER=False,
+    PLOT_REWARDS=True,
+    VERBOSE=True,
+    TENSORBOARD_LOG=True,
+    SHOW_PLOTS=False,
+)
+causal_ppo_model.save(
+    checkpoints_path.joinpath(
+        f"causal_pg_model_{datetime.datetime.now():%d%m%y%H%M%S}.pt"
+    )
+)
 
 vpg_model.load("logs/12072020180721/VPG_HIV-v0.pt")
 causal_pg_model.load("logs/12072020190532/CausalPG_HIV-v0.pt")
 ppo_model.load("logs/12072020182111/PPO_HIV-v0.pt")
 causal_ppo_model.load("logs/12072020183315/CausalPPO_HIV-v0.pt")
+
+agents = {
+    "vpg": vpg_model,
+    "causal_pg": causal_pg_model,
+    "ppo": ppo_model,
+    "causal_ppo": causal_ppo_model,
+}
+
+for name, a in agents.items():
+    plot_agent_behaviors(
+        {name: a},
+        env,
+        state_names=wn.hiv.State.variable_names(),
+        max_timesteps=100,
+        save_path=Path("logs").joinpath(
+            f"{name}_alone_behavior_{datetime.datetime.now():%d%m%y%H%M%S}.png"
+        ),
+        show_plot=False,
+    )
+    plot_agent_behaviors(
+        {
+            "random": RandomPolicy(),
+            "max": MaxTreatmentPolicy(),
+            "none": NoTreatmentPolicy(),
+            name: a,
+        },
+        env,
+        state_names=wn.hiv.State.variable_names(),
+        max_timesteps=100,
+        save_path=Path("logs").joinpath(
+            f"{name}_behavior_{datetime.datetime.now():%d%m%y%H%M%S}.png"
+        ),
+        show_plot=False,
+    )
 
 plot_agent_behaviors(
     {
@@ -123,7 +157,7 @@ plot_agent_behaviors(
     state_names=wn.hiv.State.variable_names(),
     max_timesteps=100,
     save_path=Path("logs").joinpath(
-        f"behavior_{datetime.datetime.now():%d%m%y%H%M%S}.png"
+        f"all_behavior_{datetime.datetime.now():%d%m%y%H%M%S}.png"
     ),
     show_plot=False,
 )
